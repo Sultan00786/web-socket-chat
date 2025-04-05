74ace16d6dc7583b86c8e9133d5c97dbceacc167
Sultan00786 <treeloversultan0987@gmail.com>
2025-04-05 23:56:25 +0530

    feat: chat fuctionality is implement using roomArr

--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -10,22 +10,22 @@ wss.on("connection", function (socket) {
   socketArr.push(socket);
 
   /**
-   * 
+   *
    * MESSAGE SCHEMA !!!
-   * 
+   *
    * message = {
    *  "type": "join",
    *  "payload": {
    *    "roomId": "123"
    *  }
    * }
-   * 
-   * OR 
-   * 
+   *
+   * OR
+   *
    * message = {
    *  "type": "chat",
    *  "payload": {
-   *    "message": "Hellow there!" 
+   *    "message": "Hellow there!"
    *   }
    * }
    */
@@ -34,26 +34,55 @@ wss.on("connection", function (socket) {
     const parseMess = JSON.parse(e.toString());
     /**
      * 1. user firstly connect with room using roomId from the massage_payload
-     * 
+     *
      * 2. after that he/she can able to communicate in that perticular room
-     * 
+     *
      */
-    if(parseMess.type == "join"){
+    if (parseMess.type == "join") {
       const roomId = parseMess.payload.roomId;
-      if(roomArr.has(roomId)) 
-        roomArr.get(roomId)?.push(socket);
-      else if(!roomArr.has(roomId))
-        roomArr.set(roomId, [socket]);
+      if (roomArr.has(roomId)) roomArr.get(roomId)?.push(socket);
+      else if (!roomArr.has(roomId)) roomArr.set(roomId, [socket]);
     }
-    else if(parseMess.type == "chat"){
-      // todo --> check user socket is prensetn in roomArr
 
-      
+    if (parseMess.type == "chat") {
+      // todo --> check user socket is prensetn in roomArr
+      let currentRoomId = "";
+
+      /**
+       * 1. find current user room_id from using there currect socket
+       *
+       * 2. using room_id we can forward message to other user which connect to that room_id
+       */
+      for (let [key, value] of roomArr) {
+        if (value.find((x) => x == socket)) {
+          currentRoomId = key;
+          break;
+        }
+      }
+
+      if (currentRoomId.length > 0) {
+        roomArr.get(currentRoomId)?.forEach((s) => {
+          s.send(parseMess.payload.message);
+        });
+      }
     }
   });
 
   socket.on("disconnect", () => {
+    // remove socket from socketArr
     socketArr.filter((s) => s != socket);
+
+    // remove socket from roomArr
+    let currentRoomId = "";
+    for (let [key, value] of roomArr) {
+      if (value.find((x) => x == socket)) {
+        currentRoomId = key;
+        break;
+      }
+    }
+    const filterArr = roomArr.get(currentRoomId)?.filter(s=>s!=socket)
+    if(filterArr)roomArr.set(currentRoomId, filterArr);
+
   });
 });
 
