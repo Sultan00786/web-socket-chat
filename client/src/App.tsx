3e878b425d5bf6acca95f6ced3042794e9a78964
Sultan00786 <treeloversultan0987@gmail.com>
2025-04-05 21:32:49 +0530

    feat: broadcasting implement in ws backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const iRef = useRef<HTMLInputElement | null>(null);
+
+  const clickHandler = () => {
+    setCount((count) => count + 1);
+    if (!socket || !iRef.current) return null;
+    socket.send(iRef.current.value);
+  };
+
+  useEffect(() => {
+    const webSocket = new WebSocket("ws://localhost:8000");
+    setSocket(webSocket);
+  }, []);
 
   return (
     <>
@@ -18,18 +31,18 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={clickHandler}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <input ref={iRef} type="text" />
+        <button onClick={clickHandler}>SEND</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -1,13 +1,16 @@
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8000 });
 
+const socketArr: WebSocket[] = [];
+
 wss.on("connection", function (socket) {
   console.log("A new client connected");
+  socketArr.push(socket);
   // socket.send("heyyyyyyyy !!!");
   socket.on("message", (e) => {
-    if (e.toString() === "ping") {
-      socket.send("pong");
-    }
+    socketArr.forEach((s) => {
+      s.send(e.toString());
+    });
   });
 });
